b0e7e73ac2d5cfb32f7dea42275a2ecaa8d93e60
OtiAnn <anniebh30@gmail.com>
2014-07-28 00:31:07 +0800

    Add Category scaffold and association with Product

--- app/models/product.rb
+++ app/models/product.rb
@@ -1,2 +1,11 @@
 class Product < ActiveRecord::Base
+  validates :name, :price, presence: true
+  validates :name, confirmation: true
+  validates :name_confirmation, presence: true
+  validates :price,
+    numericality: {
+      greater_than: 0,
+      less_than: 10000
+    }
+  belongs_to :category
 end
